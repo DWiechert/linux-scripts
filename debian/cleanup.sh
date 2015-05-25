09c46860cafd5aa37e9559a56b28bd8aad38304c
Dan Wiechert <dan.wiechert@gmail.com>
2015-05-25 17:41:49 -0500

    Update cleanup.sh

--- debian/cleanup.sh
+++ debian/cleanup.sh
@@ -3,7 +3,7 @@
 echo "Starting Cleaning Up"
 echo "========================================"
 sudo apt-get -f install
-sudo apt-get autoremove
+sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 sudo apt-get -y clean
 echo "========================================"
