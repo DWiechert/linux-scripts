76e896fe55ed60ac7c7e31683476e2d891031998
Dan Wiechert <dan.wiechert@gmail.com>
2016-07-21 11:09:45 -0500

    Create git-pull.bat

---
+++ windows/git-pull.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem This batch file does a "git pull" in all sub-directories of a specified directory
+rem Usage: GitPull.bat <parent directory of Git projects"
+
+pushd %1
+for /D %%i in (*) do (
+	cd %%i
+	echo "%%i"
+	git pull
+	cd ..
+)
+popd
