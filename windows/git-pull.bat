4e4b330440b4e30e5f1452c745e27d714628edc4
Dan Wiechert <dan.wiechert@gmail.com>
2017-02-28 08:52:22 -0600

    Update git-pull.bat

--- windows/git-pull.bat
+++ windows/git-pull.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem This batch file does a "git pull" in all sub-directories of a specified directory
-rem Usage: GitPull.bat <parent directory of Git projects"
+rem Usage: GitPull.bat <parent directory of Git projects>
 
 pushd %1
 for /D %%i in (*) do (
