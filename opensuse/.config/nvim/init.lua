240e49fdeb276c2a03a16efab1c4d75cc071df7c
Dan Wiechert <dan.wiechert@gmail.com>
2026-01-31 13:47:30 -0600

    Create init.lua

---
+++ opensuse/.config/nvim/init.lua
@@ -0,0 +1,36 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Basic settings
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.termguicolors = true
+
+-- Leader key
+vim.g.mapleader = " "
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  -- Color scheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+})
